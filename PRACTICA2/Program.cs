247713b4daf55925609a6ef68adc89273aa9a77e
stephaniecatalino71-prog <stephaniecatalino71@gmail.com>
2026-02-14 15:50:36 -0400

    Create Program.cs
    PRACTICA2

---
+++ PRACTICA2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even number");
+        }
+        else
+        {
+            Console.WriteLine("Odd number");
+
+        }
+
+        Console.ReadLine();
+    }
+}
